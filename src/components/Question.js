840b2114368f9e80a8dc8b37360bb65a5edd1755
moin2672 <moin2672@gmail.com>
2023-12-09 19:17:30 +0530

    first commit

---
+++ src/components/Question.js
@@ -0,0 +1,17 @@
+import Answer from "./Answer";
+
+const Question = () => {
+  return (
+    <div>
+      <div className="question">Text of our question</div>
+      <div className="answers">
+        <Answer />
+        <Answer />
+        <Answer />
+        <Answer />
+      </div>
+    </div>
+  );
+};
+
+export default Question;
