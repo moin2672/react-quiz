def544d37006313ba1defcd2c31f869d1ee843bc
moin2672 <moin2672@gmail.com>
2023-12-10 12:50:08 +0530

    2. useReducer Hook

--- src/App.js
+++
@@ -1,5 +0,0 @@
-function App() {
-  return <div>Hello React quiz</div>;
-}
-
-export default App;
--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,12 +1,38 @@
+import { useReducer } from "react";
 import Question from "./Question";
 
+const initialState = {
+  currentQuestionIndex: 0,
+  questions: [],
+};
+
+const reducer = (state, action) => {
+  if (action.type === "NEXT_QUESTION") {
+    return {
+      ...state,
+      currentQuestionIndex: state.currentQuestionIndex + 1,
+    };
+  }
+  return state;
+};
+
 const Quiz = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  console.log("state", state);
+  // const [questions, setQuestion] = useState([]);
+  // const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  // console.log("currentQuestionIndex", currentQuestionIndex);
   return (
     <div className="quiz">
       <div>
         <div className="score">Question 1/8</div>
         <Question />
-        <div className="next-button">Next question</div>
+        <div
+          className="next-button"
+          onClick={() => dispatch({ type: "NEXT_QUESTION" })}
+        >
+          Next question
+        </div>
       </div>
     </div>
   );
